43c7cc6467bd462853a0b34e21a373f641553049
Moon-YJ <tunjeong@gmail.com>
2024-02-20 21:59:51 +0900

    진행률을 활용하여 반복모션 제어

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-const speed = 1000;
+const speed = 500;
 const targetVal = 100;
 let num = 0;
 let startTime = 0;
@@ -8,16 +8,20 @@ let count = 0;
 
 btn.addEventListener('click', () => {
 	startTime = performance.now();
-	console.log('시작 시간', startTime);
 	requestAnimationFrame(move);
 });
 
 function move(time) {
 	// requestAnimationFrame에 의해서 콜백함수가 반복돌때마다의 누적시간값이 인수로 전달됨
 	let timelast = time - startTime;
-	console.log('반복 사이클마다 누적시간', timelast);
-	num++;
-	box.style.marginLeft = num + 'px';
-	if (num >= targetVal) return;
-	requestAnimationFrame(move);
+	let progress = timelast / speed;
+	console.log('누적시간', timelast);
+	console.log('진행률', progress);
+	console.log('반복횟수', count++);
+
+	progress < 0 && (progress = 0);
+	progress > 1 && (progress = 1);
+	progress < 1 && requestAnimationFrame(move);
+
+	box.style.marginLeft = targetVal * progress + 'px';
 }
