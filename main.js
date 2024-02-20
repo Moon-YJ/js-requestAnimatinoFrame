c05f43acd1143e3d31d1c388e7b5b25f9ae64f85
Moon-YJ <tunjeong@gmail.com>
2024-02-20 21:47:52 +0900

    반복 사이클마다 누적시간 구하기

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
+const speed = 1000;
+const targetVal = 100;
 let num = 0;
 let startTime = 0;
+let count = 0;
 
 btn.addEventListener('click', () => {
 	startTime = performance.now();
@@ -9,9 +12,12 @@ btn.addEventListener('click', () => {
 	requestAnimationFrame(move);
 });
 
-function move() {
+function move(time) {
+	// requestAnimationFrame에 의해서 콜백함수가 반복돌때마다의 누적시간값이 인수로 전달됨
+	let timelast = time - startTime;
+	console.log('반복 사이클마다 누적시간', timelast);
 	num++;
 	box.style.marginLeft = num + 'px';
-	if (num >= 100) return;
+	if (num >= targetVal) return;
 	requestAnimationFrame(move);
 }
