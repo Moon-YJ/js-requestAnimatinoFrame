210f1358ae4b7b33c55a777cfd3ef5d1eb48610e
Moon-YJ <tunjeong@gmail.com>
2024-02-20 22:06:35 +0900

    anime함수로 분리

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-const speed = 500;
-const targetVal = 100;
-let num = 0;
-let startTime = 0;
-let count = 0;
 
 btn.addEventListener('click', () => {
-	startTime = performance.now();
-	requestAnimationFrame(move);
+	anime(box, {
+		prop: 'margin-left',
+		value: 100,
+		duration: 1000,
+		callback: () => {
+			anime(box, {
+				prop: 'margin-top',
+				value: 200,
+				duration: 500,
+			});
+		},
+	});
 });
 
-function move(time) {
-	// requestAnimationFrame에 의해서 콜백함수가 반복돌때마다의 누적시간값이 인수로 전달됨
-	let timelast = time - startTime;
-	let progress = timelast / speed;
-	console.log('누적시간', timelast);
-	console.log('진행률', progress);
-	console.log('반복횟수', count++);
+function anime(selector, option) {
+	startTime = performance.now();
+	requestAnimationFrame(move);
 
-	progress < 0 && (progress = 0);
-	progress > 1 && (progress = 1);
-	progress < 1 && requestAnimationFrame(move);
+	function move(time) {
+		// requestAnimationFrame에 의해서 콜백함수가 반복돌때마다의 누적시간값이 인수로 전달됨
+		let timelast = time - startTime;
+		let progress = timelast / option.duration;
 
-	box.style.marginLeft = targetVal * progress + 'px';
+		progress < 0 && (progress = 0);
+		progress > 1 && (progress = 1);
+		progress < 1
+			? requestAnimationFrame(move)
+			: option.callback && option.callback();
+
+		selector.style[option.prop] = option.value * progress + 'px';
+	}
 }
